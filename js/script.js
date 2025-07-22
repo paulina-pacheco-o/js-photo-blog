91fb235dcbb1d578625f92c1e7cdf71285070788
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-22 16:14:50 +0200

    first milestone 3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-const columns = document.getElementById(`columns`);
+const cards = document.getElementById(`columns`);
+
 
 
 for (let i = 0; i < 6; i++) {
   axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then(response => {
-    console.log(response.data);
-    columns.innerHTML += `<div class="img text-center">
+    cards.innerHTML += `<div class="col-3 ps- 3 bg-white pt-4">
+          <div class="img text-center">
             ${response.data}
           </div>
           <div class="text pt-1 ps-3">
             ${response.data}
-          </div>`
+          </div>
+        </div>`
   })
+  cards.innerHTML
 }
